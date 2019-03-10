89ee23ff6ecd525c22f694fdeb6d545c948cf3d4
Vaibhav Sharma <vbvsharma.work@gmail.com>
2019-03-10 08:04:42 +0530

    Solved Conditional Statements

---
+++ C++/Conditional Statements/solution.cpp
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if (n == 1) {
+        cout << "one\n";
+    } else if (n == 2) {
+        cout << "two\n";
+    } else if (n == 3) {
+        cout << "three\n";
+    } else if (n == 4) {
+        cout << "four\n";
+    } else if (n == 5) {
+        cout << "five\n";
+    } else if (n == 6) {
+        cout << "six\n";
+    } else if (n == 7) {
+        cout << "seven\n";
+    } else if (n == 8) {
+        cout << "eight\n";
+    } else if (n == 9) {
+        cout << "nine\n";
+    } else if (n > 9) {
+        cout << "Greater than 9\n";
+    }
+
+    return 0;
+}
